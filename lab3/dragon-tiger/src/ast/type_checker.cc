22f9eb0701df5158f759ad8092522046daf2386a
gmateu <geoffroy.mateu@telecom-paris.fr>
2020-05-22 01:20:48 +0200

    ajout de quelques nodes

--- lab3/dragon-tiger/src/ast/type_checker.cc
+++ lab3/dragon-tiger/src/ast/type_checker.cc
@@ -13,5 +13,81 @@ void TypeChecker::visit(StringLiteral &literal) {
   literal.set_type(t_string);
 }
 
+void TypeChecker::visit(Sequence &seq)
+{
+  std::vector<Expr *> exprs = seq.get_exprs();
+
+  Type type = t_void;
+
+  for (Expr * expr : exprs){
+    expr->accept(this*);
+    type = expr->get_type(); // same type as their last expression
+  }
+
+  seq.set_type(type);
+}
+
+void TypeChecker::visit(IfThenElse &ite){
+  Expr cond = ite.get_condition();
+  Expr then = ite.get_then_part();
+  Expr _else = ite.get_else_part();
+
+  cond.accept(*this);
+
+  if (cond.get_type()!= t_int){
+    utils::error(ite.loc, "Type for condition must be int.")
+  }
+
+  then.accept(*this);
+  _else.accept(*this);
+
+  if (then.get_type()!=_else.get_type()){
+    utils::error(ite.loc, "Branches type not compatible.");
+  }
+
+  ite.set_type(_else.get_type());
+}
+
+void TypeChecker::visit(Let &let){
+
+}
+
+void TypeChecker::visit(VarDecl &decl){
+
+}
+
+void TypeChecker::visit(BinaryOperator &op){
+
+}
+
+void TypeChecker::visit(Identifier &id){
+
+}
+
+void TypeChecker::visit(Assign &assign){
+
+}
+
+void TypeChecker::visit(WhileLoop &loop){
+
+}
+
+void TypeChecker::visit(ForLoop &loop){
+
+}
+
+void TypeChecker::visit(Break &b){
+
+}
+
+void TypeChecker::visit(FunDecl &decl){
+
+}
+
+void TypeChecker::visit(FunCall &call){
+
+}
+
+
 } // namespace type_checker
 } // namespace ast
