51723ff30bad1078abd9f80341afde0d15771775
gmateu <geoffroy.mateu@telecom-paris.fr>
2020-05-22 02:15:35 +0200

    added few more

--- lab3/dragon-tiger/src/ast/type_checker.cc
+++ lab3/dragon-tiger/src/ast/type_checker.cc
@@ -48,43 +48,96 @@ void TypeChecker::visit(IfThenElse &ite){
   ite.set_type(_else.get_type());
 }
 
-void TypeChecker::visit(Let &let){
+void TypeChecker::visit(Let &let)
+{
+  std::vector<Decl *> &decls = let.get_decls();
+  Sequence &seq = let.get_sequence();
+  
 
+  for (Decl *decl : decls)
+  {
+    decl->accept(this *);
+  }
+
+  seq.accept(this*);
+  let.set_type(seq.get_type());
 }
 
 void TypeChecker::visit(VarDecl &decl){
 
+
+
+
 }
 
 void TypeChecker::visit(BinaryOperator &op){
+  Expr left = op.get_left();
+  Expr right = op.get_right();
+  left.accept(this*);
+  right.accept(this*);
 
+  if (left.get_type()!=right.get_type()){
+    utils:error(op.loc, "Operands do not have the same type.");
+  }
+
+  if (left.get_type() == t_void){
+    utils::error(op.loc, "Wrong type for operand.")
+  }
+
+  op.set_type(t_int);
 }
 
 void TypeChecker::visit(Identifier &id){
-
+  optional<VarDecl &> decl = id.get_decl();
+  if(decl){
+    id.set_type(decl.value().get_type());
+  }
+  
 }
 
 void TypeChecker::visit(Assign &assign){
+  Identifier lhs = assign.get_lhs();
+  Identifier rhs = assign.get_rhs();
 
+  lhs.accept(this*);
+  rhs.accept(this*);
+
+  if (lhs.get_type()!=rhs.get_type()){
+    utils::error(assign.loc, "Wrong type.")
+  }
+
+  assign.set_type(t_void);
 }
 
 void TypeChecker::visit(WhileLoop &loop){
+
+
+
 
 }
 
 void TypeChecker::visit(ForLoop &loop){
 
+
+
+
 }
 
 void TypeChecker::visit(Break &b){
-
+  b.set_type(t_void);
 }
 
 void TypeChecker::visit(FunDecl &decl){
 
+
+
+
 }
 
 void TypeChecker::visit(FunCall &call){
+
+
+
 
 }
 
